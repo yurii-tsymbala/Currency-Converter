05e699840ff718a5b5c351aa4febfbbe39b6f089
Yurii <zumbala2013@gmail.com>
2023-11-22 00:27:23 +0200

    Fix exchange result

--- converter/src/app/service/exchange.service.ts
+++ converter/src/app/service/exchange.service.ts
@@ -10,13 +10,17 @@ export class ExchangeService {
 
   convertFromTo(fromMoney: Money, toMoney: Money) {
     const link = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromMoney.currency}/${toMoney.currency}.json`;
-    return new Promise<string>(function (myResolve, myReject) {
+    return new Promise<Money>(function (myResolve, myReject) {
       let xhr = new XMLHttpRequest();
       xhr.open('GET', link);
       xhr.onload = function () {
         if (xhr.status === 200) {
           const data = JSON.parse(xhr.response);
-          console.log(data);
+          const exchangeRate = Number(data[toMoney.currency]);
+          const moneyAmount = Number(fromMoney.amount);
+          const result = exchangeRate * moneyAmount;
+          myResolve(new Money(result.toString(), toMoney.currency));
+          console.log(result);
         } else {
           myReject('Error');
         }
