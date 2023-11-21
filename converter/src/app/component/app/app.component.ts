3de588839d366d4e454b53fef4a094b0520a4242
Yurii <zumbala2013@gmail.com>
2023-11-22 00:36:19 +0200

    Configure both forms updates after user changing values

--- converter/src/app/component/app/app.component.ts
+++ converter/src/app/component/app/app.component.ts
@@ -11,16 +11,21 @@ export class AppComponent {
   leftMoney = new Money('', 'usd');
   rightMoney = new Money('', 'uah');
 
-  constructor(private service: ExchangeService) {
+  constructor(private service: ExchangeService) {}
+
+  async leftFormChanged(money: Money) {
+    const updatedMoney = await this.service.convertFromTo(
+      money,
+      this.rightMoney
+    );
+    this.rightMoney = updatedMoney;
   }
 
-  leftFormChanged(money: Money) {
-    this.service.convertFromTo(money,this.rightMoney);
-    // this.rightMoney = money;
-  }
-
-  rightFormChanged(money: Money) {
-    this.service.convertFromTo(money,this.leftMoney);
-    // this.leftMoney = money;
+  async rightFormChanged(money: Money) {
+    const updatedMoney = await this.service.convertFromTo(
+      money,
+      this.leftMoney
+    );
+    this.leftMoney = updatedMoney;
   }
 }
