f73e49eb28294ea40cd24d7a284fc53fecdc8e12
Yurii <zumbala2013@gmail.com>
2023-11-22 14:11:12 +0200

    Fix naming

--- converter/src/app/component/form/form.component.ts
+++ converter/src/app/component/form/form.component.ts
@@ -7,17 +7,17 @@ import { Money } from 'src/app/model/money';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-  @Output() valueChanged = new EventEmitter<Money>();
+  @Output() moneyChanged = new EventEmitter<Money>();
   @Input() money!: Money;
   currencies: String[] = ['usd', 'uah', 'eur', 'pln'];
 
   constructor() {}
 
-  getAmount(newValue: string) {
-    this.valueChanged.emit(this.money);
+  amountChanged() {
+    this.moneyChanged.emit(this.money);
   }
 
-  getCurrency(newValue: string) {
-    this.valueChanged.emit(this.money);
+  currencyChanged() {
+    this.moneyChanged.emit(this.money);
   }
 }
